8e5e40d0f77fe8569e8e380cc6ff0a670d8190b2
Henrry Bourgeot <henrrybrgt@gmail.com>
2022-08-27 14:09:17 -0400

    Added Docker support

--- Dockerfile
+++ Dockerfile
@@ -1,18 +1,11 @@
-FROM golang
+FROM golang:alpine AS build
+ENV GOPROXY=https://proxy.golang.org
 
-WORKDIR /app
+WORKDIR /go/src/portfolio
 
-COPY go.mod ./
-COPY go.sum ./
-RUN go mod download
+COPY . .
+RUN GOOS=linux go build -o /go/bin/portfolio cmd/web/*
 
-COPY ./cmd/ ./
-COPY ./internal/models/forms.go ./
-
-RUN go build cmd/web
-
-EXPOSE 4000
-
-
-
-CMD ["/server"]
+FROM alpine
+COPY --from=build /go/bin/portfolio /go/bin/portfolio
+ENTRYPOINT [ "go/bin/portfolio" ]
--- cmd/web/main.go
+++ cmd/web/main.go
@@ -3,13 +3,11 @@ package main
 import (
 	"log"
 	"net/http"
-	"os"
 
 	forms "github.com/hbourgeot/portfolio/internal/models"
 )
 
 func main() {
-	port := os.Getenv("PORT")
 	forms.ConnectToDB()
 	fileServer := http.FileServer(http.Dir("./ui/static/"))
 	http.Handle("/static/", http.StripPrefix("/static/", fileServer))
@@ -19,8 +17,8 @@ func main() {
 	http.HandleFunc("/admin/panel", ShowPanel)
 	http.HandleFunc("/submit", SendForm)
 	http.HandleFunc("/login", Login)
-	log.Printf("Starting server on port %s", port)
-	err := http.ListenAndServe(":"+port, nil)
+	log.Printf("Starting server on port :4000")
+	err := http.ListenAndServe(":4000", nil)
 	if err != nil {
 		log.Fatal(err)
 		return
